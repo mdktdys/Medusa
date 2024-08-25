1c6437e7100d56b3fa9a70020215a91bfe502de1
EventGamer67 <islamoffdanil67@gmail.com>
2024-08-25 12:02:16 +0500

    Add teachers in search

--- src/api_v1/search/crud.py
+++ src/api_v1/search/crud.py
@@ -1,12 +1,49 @@
+from typing import List
 from sqlalchemy.engine import Result
-from sqlalchemy import *
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-
+import asyncio
 from src.alchemy import database
+from src.api_v1.search.schemas import SearchResult
 
 
-async def get_search_items(session: AsyncSession, search_filter: str):
-    query = select(database.Groups).where(database.Groups.name.icontains(search_filter))
-    result: Result = await session.execute(query)
-    groups = list(result.scalars().all())
-    return groups
+async def get_search_items(
+    session: AsyncSession, search_filter: str
+) -> List[SearchResult]:
+    async def search_groups():
+        query = select(database.Groups).where(
+            database.Groups.name.icontains(search_filter)
+        )
+        result: Result = await session.execute(query)
+        return list(result.scalars().all())
+
+    async def search_teachers():
+        query = select(database.Teachers).where(
+            database.Teachers.name.icontains(search_filter)
+        )
+        result: Result = await session.execute(query)
+        return list(result.scalars().all())
+
+    # Запускаем оба запроса параллельно
+    groups_task, teachers_task = await asyncio.gather(
+        search_groups(), search_teachers()
+    )
+
+    groups = groups_task
+    teachers = teachers_task
+
+    # Объединяем результаты поиска групп и преподавателей
+    search_results = groups + teachers
+
+    # Создаем список SearchResult с использованием спискового включения
+    res = [
+        SearchResult(
+            search_type="group" if isinstance(i, database.Groups) else "teacher",
+            search_id=i.id,
+            search_name=i.name,
+            search_image="https://abtechsolutions.ca/wp-content/uploads/2019/02/Test.png",
+        )
+        for i in search_results
+    ]
+
+    return res
--- src/api_v1/search/views.py
+++ src/api_v1/search/views.py
@@ -1,8 +1,11 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from fastapi_cache.decorator import cache
 
 from src.alchemy.db_helper import *
 from . import crud
+from .schemas import SearchResult
 
 router = APIRouter(tags=["search"])
 
@@ -11,5 +14,5 @@ router = APIRouter(tags=["search"])
 @cache(expire=600)
 async def get_search(
     query: str, session: AsyncSession = Depends(db_helper.session_dependency)
-):
+) -> List[SearchResult]:
     return await crud.get_search_items(session=session, search_filter=query)
