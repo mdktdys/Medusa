fc7a081c8661fad319e734097d5852eba34ac1d0
EventGamer67 <islamoffdanil67@gmail.com>
2024-08-25 08:52:49 +0500

    Advanced schedule

--- src/api_v1/groups/crud.py
+++ src/api_v1/groups/crud.py
@@ -77,14 +77,24 @@ async def get_group_day_schedule_by_date_formatted(
     for para in schedule.paras:
         if para.zamena is not None:
             if para.origin is None:
-                rows.append(f"{para.zamena.number} {para.zamena.Courses_.fullname} 🔄️")
-            else:
-                rows.append(f"{para.zamena.number} {para.zamena.Courses_.fullname} 🔄️")
                 rows.append(
-                    f"<s>{para.origin.number} {para.origin.Courses_.fullname}</s>"
+                    f"\n{para.zamena.number} {para.zamena.Courses_.fullname} {para.zamena.Teachers_.name}🔄️"
+                    f" 🚪{para.zamena.Cabinets_.name}"
+                )
+            else:
+                rows.append(
+                    f"\n{para.zamena.number} {para.zamena.Courses_.fullname} {para.zamena.Teachers_.name}🔄️"
+                    f" 🚪{para.zamena.Cabinets_.name}"
+                )
+                rows.append(
+                    f"<s>{para.origin.number} {para.origin.Courses_.fullname} {para.origin.Teachers_.name}"
+                    f" 🚪{para.origin.Cabinets_.name}</s>"
                 )
         else:
-            rows.append(f"{para.origin.number} {para.origin.Courses_.fullname}")
+            rows.append(
+                f"\n{para.origin.number} {para.origin.Courses_.fullname} {para.origin.Teachers_.name}"
+                f" 🚪{para.origin.Cabinets_.name}"
+            )
     return DayScheduleFormatted(paras=rows, search_name=schedule.search_name)
 
 
