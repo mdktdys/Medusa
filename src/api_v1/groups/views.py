8b05254fc1f480c0f66c45aeb89541ebb80642b6
EventGamer67 <islamoffdanil67@gmail.com>
2024-09-06 20:06:37 +0500

    test cach

--- src/api_v1/groups/views.py
+++ src/api_v1/groups/views.py
@@ -13,13 +13,13 @@ router = APIRouter(tags=["Groups"])
 
 
 @router.get("/", response_model=list[Group])
-@cache(expire=600)
+@cache()
 async def get_groups(session: AsyncSession = Depends(db_helper.session_dependency)):
     return await crud.get_groups(session=session)
 
 
 @router.get("/id/{group_id}/", response_model=list[Group])
-@cache(expire=600)
+@cache()
 async def get_group_by_id(
     group_id: int = -1,
     session: AsyncSession = Depends(db_helper.session_dependency),
@@ -28,7 +28,7 @@ async def get_group_by_id(
 
 
 @router.get("/day_schedule/{group_id}/{date}/", response_model=DaySchedule)
-@cache(expire=600)
+@cache()
 async def get_group_day_schedule_by_date(
     group_id: int = -1,
     date: datetime = datetime.now(),
@@ -42,7 +42,7 @@ async def get_group_day_schedule_by_date(
 @router.get(
     "/day_schedule_formatted/{group_id}/{date}/", response_model=DayScheduleFormatted
 )
-@cache(expire=600)
+@cache()
 async def get_group_day_schedule_by_date_formatted(
     group_id: int = -1,
     date: datetime = datetime.now(),
@@ -56,7 +56,7 @@ async def get_group_day_schedule_by_date_formatted(
 @router.get(
     "/week_schedule/{group_id}/{monday_date}/", response_model=List[DaySchedule]
 )
-@cache(expire=600)
+@cache()
 async def get_group_week_schedule_by_date(
     group_id: int = -1,
     monday_date: datetime = datetime.now(),
