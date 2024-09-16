ac7cafd005de8e68cfbab6a13495fb872021db47
EventGamer67 <islamoffdanil67@gmail.com>
2024-09-16 09:56:40 +0500

    auth

--- router.py
+++ router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from src.api_v1 import router as api_v1_router
-from src.auth import router as auth_router
+from src.auth.auth import router as auth_router
 
 router = APIRouter()
 router.include_router(auth_router, prefix="/auth")
