9b84d27646a4024c6cb3446eb4a125f81e2d58c2
EventGamer67 <islamoffdanil67@gmail.com>
2024-09-22 01:21:12 +0500

    hide to env database url

--- src/core/config.py
+++ src/core/config.py
@@ -1,12 +1,12 @@
+import os
+
 from pydantic_settings import BaseSettings
 from pathlib import Path
 
 
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
-    db_url: str = (
-        "postgresql+asyncpg://postgres.ojbsikxdqcbuvamygezd:iuZu80DH0u6eCXj9@aws-0-eu-west-2.pooler.supabase.com:5432/postgres"
-    )
+    db_url: str = os.environ.get("SUPABASE_DATABASE_CONNECTION")
     db_echo: bool = True
     app_name: str = "Medusa"
     items_per_user: int = 50
