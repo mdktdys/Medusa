8413354c2cf667e1e9c560ae16ccf245a033732a
EventGamer67 <islamoffdanil67@gmail.com>
2024-09-22 01:49:42 +0500

    fix

--- src/core/config.py
+++ src/core/config.py
@@ -6,7 +6,9 @@ from pathlib import Path
 
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
-    db_url: str = os.environ.get("SUPABASE_DATABASE_CONNECTION")
+    db_url: str = (
+        "postgresql+asyncpg://postgres.ojbsikxdqcbuvamygezd:KCQjsy4bJiYqXxL9@aws-0-eu-west-2.pooler.supabase.com:5432/postgres"
+    )
     db_echo: bool = True
     app_name: str = "Medusa"
     items_per_user: int = 50
