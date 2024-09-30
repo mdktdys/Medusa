febf1dccf8bb8084b9a5bee6ee80e78bfe274019
EventGamer67 <islamoffdanil67@gmail.com>
2024-10-01 00:57:30 +0500

    reqs

--- src/api_v1/parser/views.py
+++ src/api_v1/parser/views.py
@@ -6,6 +6,7 @@ from fastapi_cache.decorator import cache
 
 from src.alchemy.db_helper import *
 from . import crud
+import docker
 
 router = APIRouter(tags=["Parser"])
 
@@ -16,3 +17,19 @@ pass_ = os.environ.get("API_SECRET")
 @cache(300)
 async def get_latest_zamena_link_celery() -> dict:
     return await crud.get_latest_zamena_link()
+
+
+@router.get("/containers")
+def get_containers():
+    client = docker.from_env()  # Используем Docker SDK для Python
+    containers = client.containers.list(all=True)  # Получаем список всех контейнеров
+    container_info = []
+    for container in containers:
+        container_info.append(
+            {
+                "name": container.name,
+                "status": container.status,
+                "image": container.image.tags,
+            }
+        )
+    return container_info
