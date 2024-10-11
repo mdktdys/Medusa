849786c5f5a9809b271ed23a8c4bff84bf7b1211
EventGamer67 <islamoffdanil67@gmail.com>
2024-10-12 04:29:18 +0500

    test alreadt founds links

--- src/parser/methods.py
+++ src/parser/methods.py
@@ -287,6 +287,7 @@ async def check_new() -> CheckResult:
                                 trace=Html.escape(str(traceback.format_exc())[0:100]),
                             )
                         )
+                print(result)
                 return result
         return CheckResult(result="Checked")
     except Exception as e:
