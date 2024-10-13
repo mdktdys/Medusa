6ab2cc1c0b52e8c384afbb00a8842a005a177906
EventGamer67 <islamoffdanil67@gmail.com>
2024-10-14 01:42:44 +0500

    test

--- src/api_v1/telegram/crud.py
+++ src/api_v1/telegram/crud.py
@@ -2,16 +2,26 @@ from src.parser.methods import sup
 
 
 def subscribe_zamena_notifications(chat_id: str, target_type: int, target_id: int):
-    result = sup.client.table("Subscribers").insert(
-        {"chat_id": chat_id, "target_type": target_type, "target_id": target_id}
-    ).execute()
+    result = (
+        sup.client.table("Subscribers")
+        .insert(
+            {"chat_id": chat_id, "target_type": target_type, "target_id": target_id}
+        )
+        .execute()
+    )
     print(result)
     print(result.dict)
 
 
 def unsubscribe_zamena_notifications(chat_id: str, target_type: int, target_id: int):
-    result = sup.client.table("Subscribers").insert(
-        {"chat_id": chat_id, "target_type": target_type, "target_id": target_id}
-    ).execute()
+    result = (
+        sup.client.table("Subscribers")
+        .delete()
+        .eq("chat_id", chat_id)
+        .eq("chat_id", chat_id)
+        .eq("target_type", target_type)
+        .eq("target_id", target_id)
+        .execute()
+    )
     print(result)
     print(result.dict)
