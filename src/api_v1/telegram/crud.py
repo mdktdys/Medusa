de8541b88650b6d34005eee3ff6b131168d31a56
EventGamer67 <islamoffdanil67@gmail.com>
2024-10-14 02:16:16 +0500

    test

--- src/api_v1/telegram/crud.py
+++ src/api_v1/telegram/crud.py
@@ -1,9 +1,23 @@
 from src.parser.methods import sup
+from starlette import status
 
 
 async def subscribe_zamena_notifications(
     chat_id: str, target_type: int, target_id: int
 ):
+    res = (
+        sup.client.table("Subscribers")
+        .select("id")
+        .eq("chat_id", chat_id)
+        .eq("chat_id", chat_id)
+        .eq("target_type", target_type)
+        .eq("target_id", target_id)
+        .execute()
+    )
+
+    if len(res.data) != 0:
+        return status.HTTP_202_ACCEPTED
+
     result = (
         sup.client.table("Subscribers")
         .insert(
@@ -13,6 +27,7 @@ async def subscribe_zamena_notifications(
     )
     print(result)
     print(result.dict)
+    return status.HTTP_201_CREATED
 
 
 async def unsubscribe_zamena_notifications(
@@ -29,3 +44,4 @@ async def unsubscribe_zamena_notifications(
     )
     print(result)
     print(result)
+    return status.HTTP_201_CREATED
