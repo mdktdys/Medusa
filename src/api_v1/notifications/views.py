aff7a2b5fe32aa62b7e616d9ba52384c6b2f4284
EventGamer67 <islamoffdanil67@gmail.com>
2024-12-03 12:12:33 +0500

    notifications

--- src/api_v1/notifications/views.py
+++ src/api_v1/notifications/views.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.alchemy import db_helper
 from src.api_v1.notifications import crud
 
 router = APIRouter(tags=["Notifications"], include_in_schema=True)
 
 
 @router.post("/send_single_message/")
-async def send_single_message(token: str, session):
+async def send_single_message(token: str, session: AsyncSession = Depends(db_helper.session_dependency)):
     return crud.send_single_message(session=session, token=token)
