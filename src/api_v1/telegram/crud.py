038d100cd36e9f511cfac23122e33af3f0fd1833
EventGamer67 <islamoffdanil67@gmail.com>
2025-01-12 06:18:08 +0500

    test

--- src/api_v1/telegram/crud.py
+++ src/api_v1/telegram/crud.py
@@ -8,6 +8,7 @@ from fastapi import HTTPException, Response, status
 
 from my_secrets import TELEGRAM_API_URL
 from src.alchemy import database
+from src.api_v1.groups.schemas import DayScheduleFormatted
 from src.parser.supabase import SupaBaseWorker
 
 sup = SupaBaseWorker()
@@ -93,10 +94,10 @@ async def telegram_send_message(chat_id: int, message: str):
 
 async def send_group_schedule_by_chat_id(chat_id: int, group_id: int, date: datetime.date, session: AsyncSession):
     from src.api_v1.groups.crud import get_group_day_schedule_by_date_formatted
-    schedule_message_formatted = await get_group_day_schedule_by_date_formatted(
+    schedule_message_formatted: DayScheduleFormatted = await get_group_day_schedule_by_date_formatted(
         group_id=group_id,
         chat_id=chat_id,
         date=date,
         session=session,
     )
-    await telegram_send_message(chat_id, schedule_message_formatted)
+    await telegram_send_message(chat_id, schedule_message_formatted.paras)
