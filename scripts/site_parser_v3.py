e34f4fbe2577d06c45d50b16a517931ac448052e
mdktdys <islamoffdanil67@gmail.com>
2025-02-20 07:54:05 +0500

    site parser ench

--- scripts/site_parser_v3.py
+++ scripts/site_parser_v3.py
@@ -18,6 +18,32 @@ from src.parser.models.zamena_table_model import ZamTable
 
 
 def get_zamena_tables():
+    def define_month(string_: str) -> int | None:
+        string_ = string_.split(' ')[0].lower()
+        months = [
+            "январь",
+            "февраль",
+            "март",
+            "апрель",
+            "май",
+            "июнь",
+            "июль",
+            "август",
+            "сентябрь",
+            "октябрь",
+            "ноябрь",
+            "декабрь",
+        ]
+        return months.index(string_) + 1
+
+    def define_year(string_: str) -> int | None:
+        string_separated = string_.split(' ')
+
+        if len(string_separated) == 1:
+            return datetime.datetime.now().year
+
+        return int(string_separated[1].lower())
+
     context = ssl.create_default_context(cafile=certifi.where())
     url = "https://uksivt.ru/"
     response = urlopen(url + quote('замены'), context=context)
@@ -29,8 +55,8 @@ def get_zamena_tables():
         rows = table.find_all('tr')
         hyper_link_texts: List[PageElement] = table.find_all('a')
         header_text = rows[0].text
-        month = 2
-        year = 2025
+        month = define_month(header_text)
+        year = define_year(header_text)
 
         for link in hyper_link_texts:
             text = link.get_text()
@@ -41,6 +67,7 @@ def get_zamena_tables():
                     zamenas.append(Zamena(link=zamena_link, date=date))
         zamena_tables.append(ZamTable(raw=table, month_index=month, year=year))
     return zamena_tables
+
 
 def parse():
     zamena_tables = get_zamena_tables()
