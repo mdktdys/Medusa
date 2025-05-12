5585217eb586774bf00b45c5f6beebcd9be7f09b
EventGamer67 <islamoffdanil67@gmail.com>
2025-05-12 11:21:37 +0500

    test 2

--- src/utils/key_builder.py
+++ src/utils/key_builder.py
@@ -1,8 +1,8 @@
 import hashlib
 from typing import Callable, Optional
-
-from starlette.requests import Request
-from starlette.responses import Response
+from fastapi import Request, Response
+from fastapi_cache import FastAPICache
+from sqlalchemy.ext.asyncio import AsyncSession
 
 def default_key_builder(
     func: Callable,
@@ -12,18 +12,22 @@ def default_key_builder(
     args: Optional[tuple] = None,
     kwargs: Optional[dict] = None,
 ) -> str:
-    from fastapi_cache import FastAPICache
-    
     print('ARGS')
     print(args)
     print('KWARGS')
     print(kwargs)
 
+    # Фильтруем из kwargs значения, которые не сериализуются (например, AsyncSession)
+    safe_kwargs = {
+        k: v for k, v in (kwargs or {}).items()
+        if not isinstance(v, AsyncSession)
+    }
+
     prefix = f"{FastAPICache.get_prefix()}:{namespace}:"
     cache_key = (
         prefix
-        + hashlib.md5(  # nosec:B303
-            f"{func.__module__}:{func.__name__}:{args}:{kwargs}".encode()
+        + hashlib.md5(
+            f"{func.__module__}:{func.__name__}:{args}:{safe_kwargs}".encode()
         ).hexdigest()
     )
     return cache_key
