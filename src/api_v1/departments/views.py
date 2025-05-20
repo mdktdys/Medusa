e8cca3ce9340ab2e62ba19374022880cacf59f43
EventGamer67 <islamoffdanil67@gmail.com>
2025-05-20 14:19:09 +0500

    caches

--- src/api_v1/departments/views.py
+++ src/api_v1/departments/views.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter, Depends
-from fastapi_cache import FastAPICache
-from fastapi_cache.decorator import cache
 
 from src.alchemy.db_helper import db_helper, AsyncSession
 from .schemas import Department, DepartmentCreate
@@ -12,13 +10,11 @@ router = APIRouter(tags=[namespace])
 
 
 @router.get("/", response_model = list[Department])
-@cache(expire=60000, namespace = namespace)
 async def get_departments(session: AsyncSession = Depends(db_helper.session_dependency)) -> list[Department]:
     return await crud.get_departments(session=session)
 
 
 @router.get("/{department_id}", response_model = Department)
-@cache(expire=60000, namespace = namespace)
 async def get_department_by_id(
     department_id: int,
     session: AsyncSession = Depends(db_helper.session_dependency),
@@ -33,10 +29,6 @@ async def update_department(
     session: AsyncSession = Depends(db_helper.session_dependency),
 ) -> Department:
     result: Department = await crud.update_department(session, department_id, data)
-    try:
-        await FastAPICache.clear(namespace = namespace)
-    except:
-        pass
     return result
 
 
@@ -46,10 +38,6 @@ async def delete_department(
     session: AsyncSession = Depends(db_helper.session_dependency),
 ) -> dict[str, str]:
     result: dict[str, str] = await crud.delete_department(session, department_id)
-    try:
-        await FastAPICache.clear(namespace = namespace)
-    except:
-        pass
     return result
 
 
@@ -59,8 +47,4 @@ async def create_department(
     session: AsyncSession = Depends(db_helper.session_dependency),
 ) -> Department:
     result: Department = await crud.create_department(session=session, data = data)
-    try:
-        await FastAPICache.clear(namespace = namespace)
-    except:
-        pass
     return result
