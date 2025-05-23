082a04e8db83410ab68d40a93b2abecae948bac9
mdktdys <islamoffdanil67@gmail.com>
2025-05-23 20:42:24 +0500

    add search item images

--- src/api_v1/search/views.py
+++ src/api_v1/search/views.py
@@ -3,16 +3,17 @@ from typing import List
 from fastapi import APIRouter, Depends
 from fastapi_cache.decorator import cache
 
-from src.alchemy.db_helper import *
+from src.alchemy.db_helper import AsyncSession, db_helper
 from . import crud
 from .schemas import SearchResult
-from ...auth.auth import any_auth_method
 
 router = APIRouter(tags=["Search"])
 
 
 @router.get("/search/{query}/")
+@cache(expire=6000)
 async def get_search(
-    query: str, session: AsyncSession = Depends(db_helper.session_dependency)
+    query: str,
+    session: AsyncSession = Depends(db_helper.session_dependency)
 ) -> List[SearchResult]:
     return await crud.get_search_items(session=session, search_filter=query)
