710e09cccdb82d366a200fe063725f045c8143f8
EventGamer67 <islamoffdanil67@gmail.com>
2025-06-25 00:13:03 +0500

    еуые

--- src/api_v1/timings/crud.py
+++ src/api_v1/timings/crud.py
@@ -6,18 +6,4 @@ from sqlalchemy import select
 
 async def get_timings(session: AsyncSession) -> list[Timing]:
     result: list[database.Timings] = list((await session.execute(select(database.Timings))).scalars().all())
-    
-    timings: list[Timing] = []
-    for time in result:
-        timing = Timing(
-            number = time.number,
-            start = time.start,
-            end = time.end,
-            saturday_start = time.saturdayStart,
-            saturday_end = time.saturdayEnd,
-            obed_end = time.obedEnd,
-            obed_start = time.obedStart,
-        )
-        timings.append(timing)
-    
-    return timings
+    return result
--- src/api_v1/timings/schemas.py
+++ src/api_v1/timings/schemas.py
@@ -1,17 +1,17 @@
 from datetime import time
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class Timing(BaseModel):
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(from_attributes=True, populate_by_name=True)
 
     number: int
-    
+
     start: time
     end: time
-    
-    saturday_start: time
-    saturday_end: time
 
-    obed_start: time
-    obed_end: time
+    saturday_start: time = Field(alias="saturdayStart")
+    saturday_end: time = Field(alias="saturdayEnd")
+
+    obed_start: time = Field(alias="obedStart")
+    obed_end: time = Field(alias="obedEnd")
