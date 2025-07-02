6f925e6d21a0f0605dcee167107d0e65573eedcc
EventGamer67 <islamoffdanil67@gmail.com>
2025-07-02 20:44:51 +0500

    add router

--- src/api_v2/router.py
+++ src/api_v2/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from .client.data.views import router as data_router
+from .client.zamena.views import router as zamena_router
 
 router = APIRouter()
 
 # public
 router.include_router(data_router, prefix="/data")
+router.include_router(zamena_router, prefix="/zamena")
