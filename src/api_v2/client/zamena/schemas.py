0bdefecabaf66546aa0f29f2be7f5ce3400f5fc9
EventGamer67 <islamoffdanil67@gmail.com>
2025-07-02 21:12:02 +0500

    test

--- src/api_v2/client/zamena/schemas.py
+++ src/api_v2/client/zamena/schemas.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel, ConfigDict
 from datetime import date
-
+from src.api_v1.zamenas_full.schemas import ZamenasFull
+from src.api_v1.zamena_file_links.schemas import ZamenaFileLink
+from src.api_v1.zamenas.schemas import Zamena
 
 class ZamenaRequest(BaseModel):
     model_config = ConfigDict(from_attributes = True)
@@ -14,7 +16,7 @@ class ZamenaResponse(BaseModel):
 
     practices: list
     cabinet_swaps: list
-    zamenas_full: list
-    zamena_file_links: list
-    zamenas: list
+    zamenas_full: list[ZamenasFull]
+    zamena_file_links: list[ZamenaFileLink]
+    zamenas: list[Zamena]
     
