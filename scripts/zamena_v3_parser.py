4a25252585a1539716180445ee11ff3f512e2101
mdktdys <islamoffdanil67@gmail.com>
2025-09-08 14:16:10 +0500

    fix imports

--- scripts/zamena_v3_parser.py
+++ scripts/zamena_v3_parser.py
@@ -1,26 +1,26 @@
-import asyncio
-import sys
-from datetime import date
-from io import BytesIO
-from pathlib import Path
+# import asyncio
+# import sys
+# from datetime import date
+# from io import BytesIO
+# from pathlib import Path
 
-sys.path.append(str(Path(__file__).resolve().parents[1]))
+# sys.path.append(str(Path(__file__).resolve().parents[1]))
 
-from src.parser.schedule.schedule_parser_v3 import parse_schedule_v3
+# from src.parser.schedule.schedule_parser_v3 import parse_schedule_v3
 
-from .session_maker import async_session_maker
+# from .session_maker import async_session_maker
 
 
-async def excecute():
-    monday_date = date(2025, 9, 8)
-    with open("samples/rasp_preps с 08.09.2025 г. (1).xls", "rb") as fh:
-        stream = BytesIO(fh.read())
+# async def excecute():
+#     monday_date = date(2025, 9, 8)
+#     with open("samples/rasp_preps с 08.09.2025 г. (1).xls", "rb") as fh:
+#         stream = BytesIO(fh.read())
 
-    async with async_session_maker() as session:
-        await parse_schedule_v3(stream = stream, session = session, monday_date = monday_date)
+#     async with async_session_maker() as session:
+#         await parse_schedule_v3(stream = stream, session = session, monday_date = monday_date)
 
-if __name__ == '__main__':
-    asyncio.run(excecute())
+# if __name__ == '__main__':
+#     asyncio.run(excecute())
         
         
         
--- src/auth/auth.py
+++ src/auth/auth.py
@@ -8,7 +8,7 @@ from fastapi_users.authentication import (
     BearerTransport,
     JWTStrategy,
 )
-from fastapi_users.db import SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from my_secrets import API_KEY, PUBLIC_API_KEY, SECRET
