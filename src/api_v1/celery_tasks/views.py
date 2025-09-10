f112b12288c20352ed9b2809579d49e788d3e876
mdktdys <islamoffdanil67@gmail.com>
2025-09-10 22:33:49 +0500

    d

--- src/api_v1/celery_tasks/views.py
+++ src/api_v1/celery_tasks/views.py
@@ -1,19 +1,11 @@
 from fastapi import APIRouter, Depends
 
-from src.alchemy.db_helper import AsyncSession, db_helper
-
 from . import crud
 from .schemas import TaskRequest, TaskResponse
 
 namespace: str = 'Tasks'
 router = APIRouter(tags=[namespace])
 
-@router.get("/")
-async def get_task(
-    request: TaskRequest = Depends(),
-    session: AsyncSession = Depends(db_helper.session_dependency)
-) -> TaskResponse:
-    return await crud.get_task(
-        session = session,
-        request = request
-    )
+@router.get('/')
+async def get_task(request: TaskRequest = Depends()) -> TaskResponse:
+    return await crud.get_task(request = request)
