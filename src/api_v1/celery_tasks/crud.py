aaf95b2743c9962b080d5082043e23be54f1b395
mdktdys <islamoffdanil67@gmail.com>
2025-09-10 22:47:38 +0500

    ф

--- src/api_v1/celery_tasks/crud.py
+++ src/api_v1/celery_tasks/crud.py
@@ -1,10 +1,9 @@
 from celery.result import AsyncResult
 
-from src.parser.tasks import parser_celery_app
-
 from .schemas import TaskRequest, TaskResponse
 
 
 async def get_task(request: TaskRequest) -> TaskResponse:
+    from src.parser.tasks import parser_celery_app
     res: AsyncResult = AsyncResult(request.task_id, app = parser_celery_app)
     return TaskResponse.from_async_result(res)
