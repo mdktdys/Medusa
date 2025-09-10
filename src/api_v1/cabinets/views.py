addb5877648c75ff9ad99e7879f14f9d37f862e7
mdktdys <islamoffdanil67@gmail.com>
2025-09-11 00:14:43 +0500

    ф

--- src/api_v1/cabinets/views.py
+++ src/api_v1/cabinets/views.py
@@ -5,21 +5,21 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.alchemy import local_db_helper
 
 from . import crud
-from .schemas import Cabinet
 
 router = APIRouter(tags=["Cabinets"])
 
 
-@router.get("/", response_model=list[Cabinet])
+@router.get("/")
 @cache(expire=6000)
 async def get_cabinets(session: AsyncSession = Depends(local_db_helper.session_dependency)):
     return await crud.get_cabinets(session=session)
 
 
-@router.get("/id/{cabinet_id}/", response_model=list[Cabinet])
+@router.get("/id/{cabinet_id}/")
 @cache(expire=6000)
 async def get_cabinet_by_id(
     cabinet_id: int = -1,
     session: AsyncSession = Depends(local_db_helper.session_dependency),
 ):
+    return await crud.get_cabinet_by_id(session=session, cabinet_id=cabinet_id)
     return await crud.get_cabinet_by_id(session=session, cabinet_id=cabinet_id)
