51873043dd8eabe1d626977d9e9cf58be426ca43
mdktdys <islamoffdanil67@gmail.com>
2025-09-12 02:08:25 +0500

    a

--- src/utils/get_remote_file_bytes.py
+++ src/utils/get_remote_file_bytes.py
@@ -14,10 +14,20 @@ def get_remote_file_bytes(link: str) -> bytes:
     
 async def get_remote_file_bytes_async(url: str) -> bytes | None:
     try:
-        async with httpx.AsyncClient(timeout = 10.0) as client:
+        async with httpx.AsyncClient(
+            timeout=10.0,
+            follow_redirects=True,
+            headers={"User-Agent": "Mozilla/5.0"}
+        ) as client:
             resp = await client.get(url)
-            if resp.status_code == 200:
+            if resp.status_code == 200 and resp.headers.get("Content-Type", "").startswith("image/"):
                 return resp.content
+            else:
+                print(
+                    "Download failed",
+                    {"status": resp.status_code, "ctype": resp.headers.get("Content-Type"),
+                     "history": [f"{h.status_code} {h.headers.get('Location')}" for h in resp.history]}
+                )
     except Exception as e:
         print(f"Ошибка при скачивании фото: {e}")
     return None
